3008517ac4abfa02c2492cc92d4336aedb442fc0
JesusAraujo <jeseazuara@gmail.com>
2023-05-19 08:35:11 +0000

    ok

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,12 +12,65 @@ from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
+#------------------------------- GET USERS -------------------------------
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users', methods=['GET'])
+def get_users():
+
+    users = User.query.all()
+    users_serialize = [user.serialize() for user in users]
+
+    return jsonify(users_serialize), 200
+
+#------------------------------- SIGN UP USERS -------------------------------
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    new_user = User(email = body['email'], password = body['password'])
+    db.session.add(new_user)
+    db.session.commit()
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "msg": "User created successfully", 
     }
 
+    return jsonify(response_body), 200
+
+
+#------------------------------- LOGIN USERS -------------------------------
+
+@api.route('/login', methods=['POST'])
+def login():
+    # Obtener los datos del usuario desde el cliente
+    body = request.get_json()
+    email = body['email']
+    password = body['password']
+
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user == None:
+        return jsonify({"msg": "User or password, Not exist!"}), 401
+
+    access_token = create_access_token(identity=user.serialize())
+
+    response_body = {
+        "msg": "Token create successfully",
+        "token": access_token
+    }
+
+    return jsonify(response_body), 200 
+
+#------------------------------- PRIVATE USERS -------------------------------
+
+@api.route("/private", methods=["GET"])
+#Verifica si tienes un token de acceso
+@jwt_required() 
+
+def protected():
+    #Accede a la identidad del usuario actual con get_jwt_identity.
+    response_body= {
+        "msg": "Permission granted",
+        "user": get_jwt_identity()
+    }
     return jsonify(response_body), 200
